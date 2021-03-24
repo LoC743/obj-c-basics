7094c7542118631317f013567ecc4cefccd93dce
Alexey Morozov <alexey1562@gmail.com>
2021-03-24 16:23:48 +0300

    [Урок 2]: Добавлена функция для провекрки вхождения буквы в английский алфавит

--- lesson-2.IsInAlphabet/lesson-2.IsInAlphabet/ViewController.m
+++ lesson-2.IsInAlphabet/lesson-2.IsInAlphabet/ViewController.m
@@ -15,7 +15,24 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view.
+    
+    NSString *character = @"z";
+    
+    if ([self isInEnglishAlphabetContains: character]) {
+        NSLog(@"[YES]: English alphabet contains letter %@", character);
+    } else {
+        NSLog(@"[NO]: English alphabet does not contains letter %@", character);
+    }
+}
+
+-(BOOL) isInEnglishAlphabetContains: (NSString *) character {
+    NSString *alphabet = @"ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+    
+    if ([alphabet containsString: character]) {
+        return YES;
+    } else {
+        return NO;
+    }
 }
 
 
