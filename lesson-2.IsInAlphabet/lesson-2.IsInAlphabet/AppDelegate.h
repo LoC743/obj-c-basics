8eb6f44b18f7d61626920f3349496a77515f78bd
Alexey Morozov <alexey1562@gmail.com>
2021-03-24 15:58:20 +0300

    [Урок 2]: Создан проект

---
+++ lesson-2.IsInAlphabet/lesson-2.IsInAlphabet/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  lesson-2.IsInAlphabet
+//
+//  Created by Alexey on 24.03.2021.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ lesson-2.IsInAlphabet/lesson-2.IsInAlphabet/ViewController.h
@@ -0,0 +1,14 @@
+//
+//  ViewController.h
+//  lesson-2.IsInAlphabet
+//
+//  Created by Alexey on 24.03.2021.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
