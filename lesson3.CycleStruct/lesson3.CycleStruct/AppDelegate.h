e90fb9e0c09bafc49365b45c63f112478fef1d56
Alexey Morozov <alexey1562@gmail.com>
2021-03-29 15:07:08 +0300

    [Урок 3]: Создан проект

---
+++ lesson3.CycleStruct/lesson3.CycleStruct/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  lesson3.CycleStruct
+//
+//  Created by Alexey on 29.03.2021.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ lesson3.CycleStruct/lesson3.CycleStruct/ViewController.m
@@ -0,0 +1,22 @@
+//
+//  ViewController.m
+//  lesson3.CycleStruct
+//
+//  Created by Alexey on 29.03.2021.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+}
+
+
+@end
