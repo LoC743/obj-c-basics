9d871403cc6d3f2f736d31d62a01ba392f6fbf10
Alexey Morozov <alexey1562@gmail.com>
2021-03-29 15:21:34 +0300

    [Урок 3]: Реализовано задание №1.

--- lesson3.CycleStruct/lesson3.CycleStruct/ViewController.m
+++ lesson3.CycleStruct/lesson3.CycleStruct/ViewController.m
@@ -16,6 +16,33 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     
+    [self loop];
+}
+
+- (void)loop {
+    NSArray *stringArray = @[@"Lorem", @"ipsum", @"dolor", @"sit", @"amet,", @"consectetur", @"adipiscing", @"elit.", @"Vivamus", @"efficitur."];
+    
+    // MARK: - For in loop
+    for (NSString *string in stringArray) {
+        NSLog(@"%@", string);
+    }
+    
+    // MARK: - For loop
+    for (NSUInteger i = 0; i < [stringArray count]; ++i) {
+        NSString *string = [stringArray objectAtIndex: i];
+//        NSString *string = stringArray[i];
+        NSLog(@"%@", string);
+    }
+    
+    // MARK: - While loop
+    NSUInteger index = 0;
+    while (index < [stringArray count]) {
+        NSString *string = [stringArray objectAtIndex: index];
+//        NSString *string = stringArray[index];
+        NSLog(@"%@", string);
+        
+        ++index;
+    }
 }
 
 
