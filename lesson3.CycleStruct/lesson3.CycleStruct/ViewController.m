afd2dee978a202fa7dde4ac5a5f538c9fec08621
Alexey Morozov <alexey1562@gmail.com>
2021-04-08 14:52:38 +0300

    [Урок 3]: Добавлен пользовательский ввод к заданию 1

--- lesson3.CycleStruct/lesson3.CycleStruct/ViewController.m
+++ lesson3.CycleStruct/lesson3.CycleStruct/ViewController.m
@@ -8,7 +8,9 @@
 #import "ViewController.h"
 
 @interface ViewController ()
+@property (weak, nonatomic) IBOutlet UITextField *textField;
 
+@property NSMutableArray *stringArray;
 @end
 
 @implementation ViewController
@@ -16,19 +18,30 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     
-    [self printLoop];
+    _stringArray = [[NSMutableArray alloc] init];
+//    NSArray *stringArray = @[@"Lorem", @"ipsum", @"dolor", @"sit", @"amet,", @"consectetur", @"adipiscing", @"elit.", @"Vivamus", @"efficitur."];
+//    [self printLoop: stringArray];
     [self printStruct];
 }
 
-- (void)printLoop {
-    NSArray *stringArray = @[@"Lorem", @"ipsum", @"dolor", @"sit", @"amet,", @"consectetur", @"adipiscing", @"elit.", @"Vivamus", @"efficitur."];
+- (IBAction)addToArrayTapped:(id)sender {
+    NSString *string = [[self textField] text];
+    NSLog(@"%@", _stringArray);
+    [_stringArray addObject: string];
+    
+    [self printLoop: _stringArray];
+}
+
+- (void)printLoop:(NSArray *) stringArray {
     
     // MARK: - For in loop
+    NSLog(@"[For in loop]");
     for (NSString *string in stringArray) {
         NSLog(@"%@", string);
     }
     
     // MARK: - For loop
+    NSLog(@"[For loop]");
     for (NSUInteger i = 0; i < [stringArray count]; ++i) {
         NSString *string = [stringArray objectAtIndex: i];
 //        NSString *string = stringArray[i];
@@ -36,6 +49,7 @@
     }
     
     // MARK: - While loop
+    NSLog(@"[While loop]");
     NSUInteger index = 0;
     while (index < [stringArray count]) {
         NSString *string = [stringArray objectAtIndex: index];
@@ -44,6 +58,17 @@
         
         ++index;
     }
+    
+    // MARK: - Do While loop
+    NSLog(@"[Do While loop]");
+    index = 0;
+    do {
+        NSString *string = [stringArray objectAtIndex: index];
+//        NSString *string = stringArray[index];
+        NSLog(@"%@", string);
+        
+        ++index;
+    } while (index < [stringArray count]);
 }
 
 
