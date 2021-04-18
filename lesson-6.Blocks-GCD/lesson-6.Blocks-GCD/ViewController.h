7c0091d9af50a04cd4a3730cc0190d96932685b4
Alexey Morozov <alexey1562@gmail.com>
2021-04-18 13:58:31 +0300

    [Урок 6]: Добавлен проект.

---
+++ lesson-6.Blocks-GCD/lesson-6.Blocks-GCD/ViewController.h
@@ -0,0 +1,14 @@
+//
+//  ViewController.h
+//  lesson-6.Blocks-GCD
+//
+//  Created by Alexey on 18.04.2021.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ lesson-6.Blocks-GCD/lesson-6.Blocks-GCD/ViewController.m
@@ -0,0 +1,22 @@
+//
+//  ViewController.m
+//  lesson-6.Blocks-GCD
+//
+//  Created by Alexey on 18.04.2021.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+
+@end
