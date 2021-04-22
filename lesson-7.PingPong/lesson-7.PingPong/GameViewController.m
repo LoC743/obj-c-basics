5802398cc816c8ac2af39e6f2a7f58711b1330bc
Alexey Morozov <alexey1562@gmail.com>
2021-04-22 10:29:16 +0300

    [Урок 7]: Property используются через self

--- lesson-7.PingPong/lesson-7.PingPong/GameViewController.m
+++ lesson-7.PingPong/lesson-7.PingPong/GameViewController.m
@@ -46,49 +46,49 @@
 }
 
 - (void)setupGrid {
-    _gridView = [[UIView alloc] initWithFrame:CGRectMake(0, HALF_SCREEN_HEIGHT - 2, SCREEN_WIDTH, 4)];
-    _gridView.backgroundColor = [[UIColor whiteColor] colorWithAlphaComponent:0.5];
-    [self.view addSubview:_gridView];
+    self.gridView = [[UIView alloc] initWithFrame:CGRectMake(0, HALF_SCREEN_HEIGHT - 2, SCREEN_WIDTH, 4)];
+    self.gridView.backgroundColor = [[UIColor whiteColor] colorWithAlphaComponent:0.5];
+    [self.view addSubview:self.gridView];
 }
 
 - (void)setupTopRacket {
-    _racketTop = [[UIImageView alloc] initWithFrame:CGRectMake(30, 40, 90, 60)];
-    _racketTop.image = [UIImage imageNamed:@"racketTop"];
-    _racketTop.contentMode = UIViewContentModeScaleAspectFit;
-    [self.view addSubview:_racketTop];
+    self.racketTop = [[UIImageView alloc] initWithFrame:CGRectMake(30, 40, 90, 60)];
+    self.racketTop.image = [UIImage imageNamed:@"racketTop"];
+    self.racketTop.contentMode = UIViewContentModeScaleAspectFit;
+    [self.view addSubview:self.racketTop];
 }
 
 - (void)setupBottomRacket {
-    _racketBottom = [[UIImageView alloc] initWithFrame:CGRectMake(30, SCREEN_HEIGHT - 90, 90, 60)];
-    _racketBottom.image = [UIImage imageNamed:@"racketBottom"];
-    _racketBottom.contentMode = UIViewContentModeScaleAspectFit;
-    [self.view addSubview:_racketBottom];
+    self.racketBottom = [[UIImageView alloc] initWithFrame:CGRectMake(30, SCREEN_HEIGHT - 90, 90, 60)];
+    self.racketBottom.image = [UIImage imageNamed:@"racketBottom"];
+    self.racketBottom.contentMode = UIViewContentModeScaleAspectFit;
+    [self.view addSubview:self.racketBottom];
 }
 
 - (void)setupBall {
-    _ball = [[UIView alloc] initWithFrame:CGRectMake(self.view.center.x - 10, self.view.center.y - 10, 20, 20)];
-    _ball.backgroundColor = [UIColor whiteColor];
-    _ball.layer.cornerRadius = 10;
-    _ball.hidden = YES;
-    [self.view addSubview:_ball];
+    self.ball = [[UIView alloc] initWithFrame:CGRectMake(self.view.center.x - 10, self.view.center.y - 10, 20, 20)];
+    self.ball.backgroundColor = [UIColor whiteColor];
+    self.ball.layer.cornerRadius = 10;
+    self.ball.hidden = YES;
+    [self.view addSubview:self.ball];
 }
 
 - (void)setupScoreTop {
-    _scoreTop = [[UILabel alloc] initWithFrame:CGRectMake(SCREEN_WIDTH - 70, HALF_SCREEN_HEIGHT - 70, 50, 50)];
-    _scoreTop.textColor = [UIColor whiteColor];
-    _scoreTop.text = @"0";
-    _scoreTop.font = [UIFont systemFontOfSize:40.0 weight:UIFontWeightLight];
-    _scoreTop.textAlignment = NSTextAlignmentCenter;
-    [self.view addSubview:_scoreTop];
+    self.scoreTop = [[UILabel alloc] initWithFrame:CGRectMake(SCREEN_WIDTH - 70, HALF_SCREEN_HEIGHT - 70, 50, 50)];
+    self.scoreTop.textColor = [UIColor whiteColor];
+    self.scoreTop.text = @"0";
+    self.scoreTop.font = [UIFont systemFontOfSize:40.0 weight:UIFontWeightLight];
+    self.scoreTop.textAlignment = NSTextAlignmentCenter;
+    [self.view addSubview:self.scoreTop];
 }
 
 - (void)setupScoreBottom {
-    _scoreBottom = [[UILabel alloc] initWithFrame:CGRectMake(SCREEN_WIDTH - 70, HALF_SCREEN_HEIGHT + 70, 50, 50)];
+    self.scoreBottom = [[UILabel alloc] initWithFrame:CGRectMake(SCREEN_WIDTH - 70, HALF_SCREEN_HEIGHT + 70, 50, 50)];
     _scoreBottom.textColor = [UIColor whiteColor];
-    _scoreBottom.text = @"0";
-    _scoreBottom.font = [UIFont systemFontOfSize:40.0 weight:UIFontWeightLight];
-    _scoreBottom.textAlignment = NSTextAlignmentCenter;
-    [self.view addSubview:_scoreBottom];
+    self.scoreBottom.text = @"0";
+    self.scoreBottom.font = [UIFont systemFontOfSize:40.0 weight:UIFontWeightLight];
+    self.scoreBottom.textAlignment = NSTextAlignmentCenter;
+    [self.view addSubview:self.scoreBottom];
 }
 
 @end
