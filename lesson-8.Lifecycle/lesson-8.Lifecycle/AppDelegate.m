e9bfb79820a96e4c2417e71e47e28a1dc50ddac2
Alexey Morozov <alexey1562@gmail.com>
2021-04-24 16:03:28 +0300

    [Урок 8]: Реализовано задание №1

--- lesson-8.Lifecycle/lesson-8.Lifecycle/AppDelegate.m
+++ lesson-8.Lifecycle/lesson-8.Lifecycle/AppDelegate.m
@@ -15,7 +15,28 @@
 
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
+    NSLog(@"[Application]: didFinishLaunchingWithOptions");
     return YES;
+}
+
+- (void)applicationWillResignActive:(UIApplication *)application {
+    NSLog(@"[Application]: applicationWillResignActive");
+}
+
+- (void)applicationDidBecomeActive:(UIApplication *)application {
+    NSLog(@"[Application]: applicationDidBecomeActive");
+}
+
+- (void)applicationDidEnterBackground:(UIApplication *)application {
+    NSLog(@"[Application]: applicationDidEnterBackground");
+}
+
+- (void)applicationWillEnterForeground:(UIApplication *)application {
+    NSLog(@"[Application]: applicationWillEnterForeground");
+}
+
+- (void)applicationWillTerminate:(UIApplication *)application {
+    NSLog(@"[Application]: applicationWillTerminate");
 }
 
 
